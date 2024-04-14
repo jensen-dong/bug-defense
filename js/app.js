2554728c0bcdf222bcd5d28f80df09d486398e46
jensen-dong <jensendong141@gmail.com>
2024-04-13 17:12:37 -0700

    feat: add key press and animations

--- js/app.js
+++ js/app.js
@@ -2,11 +2,14 @@
 const game = document.getElementById('game');
 const score = document.getElementById('score');
 const cash = document.getElementById('cash');
-let player;
-let turret1;
-let turret2;
-let turret3;
-let bug;
+const input = {
+    w: {
+        down: false
+    },
+    s: {
+        down: false
+    },
+}
 const turretBtn1 = document.getElementById('buy-turret-1');
 const turretBtn2 = document.getElementById('buy-turret-2');
 const turretBtn3 = document.getElementById('buy-turret-3');
@@ -21,23 +24,114 @@ ctx.drawImage(bgi, 0, 0);
 
 
 //====== CRAWLER CLASS ======
-class Crawler {
-    constructor(x, y, color, width, height) {
-      this.x = x;
-      this.y = y;
-      this.color = color;
-      this.height = height;
-      this.width = width;
-      this.alive = true;
-  
-      this.render = function () {
-        ctx.fillStyle = this.color; // change the color of the context (ctx)
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-      };
+class Sprite {
+    constructor({position, velocity}) {
+      this.position = position;
+      this.velocity = velocity;
+      this.height = 150;
+      this.attackPosition = {
+        position: this.position,
+        width: 100,
+        height: 50,
+      }
     }
-  }
 
-class Turret {
+    draw() {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.position.x, this.position.y, 50, this.height);
+
+        ctx.fillRect(this.attackPosition.x, this.attackPosition.y)
+    }
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+
+        if (this.position.y + this.height + this.velocity.y >= game.height) {
+            this.velocity.y = 0;
+        }
+    }
+}
+
+const player = new Sprite({
+    position: {
+    x: 40,
+    y: 300
+    },
+    velocity: {
+        x: 0,
+        y: 10
+    }
+});
+
+
+
+const bug = new Sprite({
+    position: {
+    x: 1000,
+    y: 300
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+});
+
+
+//====== create animation function ======
+
+
+function animate() {
+    window.requestAnimationFrame(animate);
+    ctx.drawImage(bgi, 0, 0);
+    //console.log('move');
+    player.update();
+    bug.update();
+
+    player.velocity.y = 0;
+
+    if (input.w.down) {
+        player.velocity.y = -3;
+    } else if (input.s.down) {
+        player.velocity.y = 3;
+    }
+}
+
+animate();
+
+//====== EVENT LISTENER ======
+window.addEventListener('DOMContentLoaded', function() {
+
+    /* donkey = new Crawler(10,20,'grey', 20, 20); */
+    //run a game loop
+    /* const runGame = this.setInterval(gameLoop, 60); */
+});
+
+window.addEventListener('keydown', (event) => {
+    console.log(event.key);
+    switch (event.key) {
+        case 'w':
+            input.w.down = true;
+            break;
+        case 's':
+            input.s.down = true;
+            break;
+    }
+});
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'w':
+            input.w.down = false;
+            break;
+        case 's':
+            input.s.down = false;
+            break;
+    }
+});
+
+/* class Turret {
     constructor(x, y, color, width, height) {
         this.x = x;
         this.y = y;
@@ -55,12 +149,11 @@ class Turret {
           ctx.strokeRect(this.x, this.y, this.width, this.height);
         };
     }
-}
+} */
 
-class Character {
-    constructor(x, y, image, width, height) {
-        this.x = x;
-        this.y = y;
+/* class Sprite {
+    constructor(position, image, width, height) {
+        this.position = position;
         this.image = image;
         this.height = height;
         this.width = width;
@@ -70,7 +163,7 @@ class Character {
             ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
         }
     }
-}
+} */
 
 //TODO: rethink trip wire methods
 
@@ -104,17 +197,6 @@ ctx.stroke(); */
 
 console.log(game.height);
 console.log(game.width);
-
-//====== EVENT LISTENER ======
-window.addEventListener('DOMContentLoaded', function() {
-
-    /* donkey = new Crawler(10,20,'grey', 20, 20); */
-    player = new Crawler(20, 170, 'yellow', 20, 100);
-    player.render();
-    //run a game loop
-    /* const runGame = this.setInterval(gameLoop, 60); */
-});
-
 
 
 
